9db914ec5489e84c85bdb84b249ab5d2e2f46bce
Himalee <himalee.tailor@gmail.com>
2019-02-28 16:15:32 +0000

    Add: ServerConnector

--- src/main/java/ServerConnector.java
+++ src/main/java/ServerConnector.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerConnector {
+    public Socket connect(ServerSocket serverSocket) throws IOException {
+        return serverSocket.accept();
+    }
 }
