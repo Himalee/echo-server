d9475948e405eff87b345ce0a88eccca0b9852ae
Himalee <himalee.tailor@gmail.com>
2019-03-05 12:16:45 +0000

    Refactor: remove duplication in tests

--- src/test/java/EchoServerTest.java
+++ src/test/java/EchoServerTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
@@ -6,22 +7,24 @@ import java.io.*;
 public class EchoServerTest {
 
     private int port = 1234;
+    private String clientInput;
+    private MockServerSocketManager mockServerSocketManager;
+
+   @Before
+   public void setUp() throws IOException {
+       clientInput = "echo";
+       mockServerSocketManager = new MockServerSocketManager(clientInput);
+       EchoServer echoServer = new EchoServer(mockServerSocketManager);
+       echoServer.start(port);
+   }
 
     @Test
-    public void startEchoServer_checkConnection() throws IOException {
-        String clientInput = "echo";
-        MockServerSocketManager mockServerSocketManager = new MockServerSocketManager(clientInput);
-        EchoServer echoServer = new EchoServer(mockServerSocketManager);
-        echoServer.start(port);
+    public void startEchoServer_checkConnection() {
         Assert.assertTrue(mockServerSocketManager.wasConnectCalled());
     }
 
     @Test
-    public void startEchoServer_sendClientMessageToServer() throws IOException {
-        String clientInput = "echo";
-        MockServerSocketManager mockServerSocketManager = new MockServerSocketManager(clientInput);
-        EchoServer echoServer = new EchoServer(mockServerSocketManager);
-        echoServer.start(port);
+    public void startEchoServer_sendClientMessageToServer() {
         Assert.assertEquals(clientInput, mockServerSocketManager.receiveString());
     }
 }
