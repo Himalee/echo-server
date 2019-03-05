530c12b8f72871b0e6bb6d13f1811ae321ea400d
Himalee <himalee.tailor@gmail.com>
2019-03-05 12:00:58 +0000

    Update: remove unsued code

--- src/main/java/ServerConnector.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class ServerConnector {
-    public Socket connect(ServerSocket serverSocket) throws IOException {
-        return serverSocket.accept();
-    }
-}
